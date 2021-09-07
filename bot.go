82c30924cd01823a4d45441ddc4f39c99ab3907e
xyths <39478565+xyths@users.noreply.github.com>
2021-09-07 11:15:22 +0800

    add Init

--- bot.go
+++ bot.go
@@ -3,12 +3,16 @@ package tgb
 import (
 	"context"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
+	"time"
 )
 
 const (
 	maxPerGroup = 20 // 20 messages per minute to the same group
+	indexName   = "expireByTime"
 )
 
 type Bot struct {
@@ -32,6 +36,10 @@ func New(token string, coll *mongo.Collection, Sugar *zap.SugaredLogger) (*Bot, 
 	}, nil
 }
 
+func (b *Bot) Init(ctx context.Context) error {
+	return b.initIndex(ctx)
+}
+
 func (b *Bot) Serve(ctx context.Context, ch <-chan tgbotapi.Chattable) error {
 	for {
 		select {
@@ -42,6 +50,37 @@ func (b *Bot) Serve(ctx context.Context, ch <-chan tgbotapi.Chattable) error {
 		}
 	}
 }
+
+func (b *Bot) initIndex(ctx context.Context) error {
+	// list index first
+	indexView := b.coll.Indexes()
+	cursor, err := indexView.List(ctx, options.ListIndexes().SetMaxTime(time.Second*2))
+	if err != nil {
+		return err
+	}
+	var indexes []bson.M
+	if err = cursor.All(ctx, &indexes); err != nil {
+		return err
+	}
+	b.Sugar.Debugf("all indexes in collection: %v", indexes)
+	for _, index := range indexes {
+		if index["name"] == indexName {
+			b.Sugar.Infof("index %s already exist", indexName)
+			return nil
+		}
+	}
+	index := mongo.IndexModel{
+		Keys:    bson.D{{"createdAt", 1}},
+		Options: options.Index().SetExpireAfterSeconds(2 * 60).SetName(indexName),
+	}
+	name, err := indexView.CreateOne(ctx, index)
+	if err != nil {
+		return err
+	}
+	b.Sugar.Infof("create index: %s", name)
+	return nil
+}
+
 func (b *Bot) send(ctx context.Context, c tgbotapi.Chattable) {
 	switch c.(type) {
 	case tgbotapi.MessageConfig:
@@ -54,9 +93,35 @@ func (b *Bot) send(ctx context.Context, c tgbotapi.Chattable) {
 }
 
 func (b *Bot) sendMessage(ctx context.Context, c tgbotapi.MessageConfig) error {
-	b.Sugar.Infof("send msg %s", c.Text)
-	if _, err := b.bot.Send(c); err != nil {
+	chatId := c.ChatID
+	count, err := b.coll.CountDocuments(ctx, bson.D{
+		{"chatId", chatId},
+	})
+
+	if err == nil && count < maxPerGroup {
+		b.Sugar.Infof("sent %d messages to chat %d", count, chatId)
+		_, err := b.bot.Send(c)
 		return err
 	}
-	return nil
+	retry := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.After(time.Second):
+			count, err := b.coll.CountDocuments(ctx, bson.D{
+				{"chatId", chatId},
+			})
+
+			if err == nil && count < maxPerGroup {
+				b.Sugar.Infof("sent %d messages to chat %d", count, chatId)
+				_, err := b.bot.Send(c)
+				return err
+			}
+			retry++
+			if retry >= 10 {
+				return nil // return anyway
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.16
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	go.mongodb.org/mongo-driver v1.7.2 // indirect
+	go.mongodb.org/mongo-driver v1.7.2
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.0 // indirect
+	go.uber.org/zap v1.19.0
 )
